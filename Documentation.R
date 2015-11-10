0f6dd47f8ad9b4045b55188e45abbf61cc40bfd5
sonip396 <jorge.espinosacam@gmail.com>
2015-11-10 18:02:33 +0100

    Assignment working

--- Documentation.R
+++ Documentation.R
@@ -37,23 +37,20 @@ nn3 <- neuralnet(X0.26 ~ X0.0625 + X0.375 + X0.9375 + X0.75 + X0.0625.1 + X0 + X
 nn3
 plot(nn3)
 nn3$result.matrix
+##Evaluation of the results##
+handwriting_results <- compute(nn3, handwriting_test[1:62])
+predicted_target <- handwriting_test
 
-nn2 <- neuralnet(X0.26 ~ X0.0625 + X0.375 + X0.9375 + X0.75 + X0.0625.1 + X0 + X0.1
-                + X0.2 + X0.4375 + X1 + X0.375.1 + X0.375.2 + X0.625 + X0.3 + X0.4
-                + X0.6 + X0.5 + X1.1 + X0.125 + X0.7 + X0.6875 + X0.125.1 + X0.8
-                + X0.9 + X0.3125 + X1.2 + X0.1875 + X0.10 + X0.3125.1 + X0.4375.1
-                + X0.11 + X0.12 + X0.4375.2 + X0.8125 + X0.1875.1 + X0.13 + X0.5.1
-                + X0.4375.3 + X0.14 + X0.25 + X0.75.1 + X0.15 + X0.0625.2 + X0.8125.1
-                + X0.3125.2 + X0.16 + X0.17 + X0.18 + X0.875 + X0.5625 + X0.9375.1
-                + X0.5625.1 + X0.19 + X0.20 + X0.21 + X0.22 + X0.375.3 + X0.875.1
-                + X0.4375.4 + X0.0625.3 + X0.23 + X0.24,
-                data = handwriting_train, hidden = c(2,3), stepmax = 10e+05, 
-                threshold = 0.05)
-nn2
-plot(nn2)
-nn2$result.matrix
-nn2$covariate
-handwriting_train$X0.26
-nn2$net.result[[1]]
-
-
+nn4 <- neuralnet(X0.26 ~ X0.0625 + X0.375 + X0.9375 + X0.75 + X0.0625.1 + X0 + X0.1
+                 + X0.2 + X0.4375 + X1 + X0.375.1 + X0.375.2 + X0.625 + X0.3 + X0.4
+                 + X0.6 + X0.5 + X1.1 + X0.125 + X0.7 + X0.6875 + X0.125.1 + X0.8
+                 + X0.9 + X0.3125 + X1.2 + X0.1875 + X0.10 + X0.3125.1 + X0.4375.1
+                 + X0.11 + X0.12 + X0.4375.2 + X0.8125 + X0.1875.1 + X0.13 + X0.5.1
+                 + X0.4375.3 + X0.14 + X0.25 + X0.75.1 + X0.15 + X0.0625.2 + X0.8125.1
+                 + X0.3125.2 + X0.16 + X0.17 + X0.18 + X0.875 + X0.5625 + X0.9375.1
+                 + X0.5625.1 + X0.19 + X0.20 + X0.21 + X0.22 + X0.375.3 + X0.875.1
+                 + X0.4375.4 + X0.0625.3 + X0.23 + X0.24,
+                 data = handwriting_train, hidden = c(4,2), learningrate = 0.05,
+                 stepmax = 1e+09)
+nn4
+plot(nn4)
